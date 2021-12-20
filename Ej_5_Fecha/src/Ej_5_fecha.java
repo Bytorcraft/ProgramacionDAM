e322d8babab7d2008ba9fcd8185441b42c745660
Bytorcraft <smr.aherrero.191@iespabloserrano.com>
2021-12-20 12:27:15 +0100

    20/12/2021

---
+++ Ej_5_Fecha/src/Ej_5_fecha.java
@@ -0,0 +1,29 @@
+/**
+ * Programa que te muestra un calendario gregoriano.
+ */
+
+/**
+ * @author Minaya Herrero
+ * @date hoy
+ */
+public class Ej_5_fecha {
+	class Fecha {
+		int dia,mes,año;
+		
+		public void setDia(int dia) {
+			this.dia=dia;
+		}
+		public void setMes(int mes) {
+			this.mes=mes;
+		}
+		public void setAño(int año) {
+			this.año=año;
+		}
+		
+	}
+	public static void main(String[] args) {
+
+		
+	}
+
+}
