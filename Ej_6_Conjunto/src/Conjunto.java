f5f3f9715991685042f458c41110b4de161f798d
Bytorcraft <smr.aherrero.191@iespabloserrano.com>
2022-02-03 14:19:03 +0100

    03/02/2022

--- Ej_6_Conjunto/src/Conjunto.java
+++ Ej_6_Conjunto/src/Conjunto.java
@@ -2,28 +2,100 @@ import java.util.*;
 public class Conjunto {
 	 
 	Scanner teclado=new Scanner(System.in);
+	int[] vector=new int [100];
+	int[] vector2=new int[100];
 	
-	public static ArrayList<Integer> generarAleatoriosNoRepetidos (int cantidad) {// genera 100 numeros
-		ArrayList<Integer> respuesta = new ArrayList<>();
-		for (int i = 0; i < 100; i++) {
-			respuesta.add(gen(respuesta));
-		}			
-		return respuesta;
+	public void crear(){// crea un vector de 100 digitos sin repetir entre el 1 y el 1000
+        for(int i=0;i<vector.length;i++){
+            boolean repetido = false;
+            int ale=(int)(Math.random()*1000)+1;
+            for (int j=0;j<i ;j++){
+                if(vector[j]==ale){
+                    repetido=true;
+                }
+            }
+            if(!repetido){
+                vector[i]=ale;
+            }else{
+                i--;
+            }
+        } 
+    }
+	public void crear2(){// crea un SEGUNDO vector de 100 digitos sin repetir entre el 1 y el 1000
+        for(int i=0;i<vector2.length;i++){
+            boolean repetido = false;
+            int ale=(int)(Math.random()*1000)+1;
+            for (int j=0;j<i ;j++){
+                if(vector2[j]==ale){
+                    repetido=true;
+                }
+            }
+            if(!repetido){
+                vector2[i]=ale;
+            }else{
+                i--;
+            }
+        } 
+    }
+	public void decirVector() {// te dice cual es el vector
+		System.out.println("El vector es: ");
+		for(int i=0;i<vector.length;i++){
+            System.out.print(vector[i]+" ");
+        }
+	}
+	public void vaciar() {
+		for (int x=0; x<vector.length; x++) {// al vector se le da el valor 0
+			vector[x]=0;
+			
+			}
+		System.out.println("Vaciado: ");
+		for(int i=0;i<vector.length;i++){
+            System.out.print(vector[i]+" ");
+        }
+	}
+	public void añadir() {// te deja añadir en la posicion que sea un nuevo numero
+		int posicion, numero;
+		System.out.println("Di en que posicion quieres cambiar el valor: ");
+		posicion=teclado.nextInt();
+		System.out.println("Di a que numero lo quieres cambiar: ");
+		numero=teclado.nextInt();
+		vector[posicion]=numero;	
+	}
+	public void eliminar() {
+		int posicion;
+		System.out.println("Di en que posicion quieres cambiar el valor: ");
+		posicion=teclado.nextInt();
+		vector[posicion]=0;		
 	}
 	
-	public static int gen(ArrayList<Integer>a) {// comprueba que no estan repetidos
-		Random ra = new Random();	
-		int numero = ra.nextInt(11);
-		if (!a.contains(numero)) {
-			return numero;
-		}else {
-			return gen(a);
+	public void copia() {
+		System.out.println("El primer vector: ");
+		for(int i=0;i<vector.length;i++){
+            System.out.print(vector[i]+" ");
+        }
+		System.out.println("");
+		System.out.println("El segundo vector: ");
+		for(int i=0;i<vector2.length;i++){
+            System.out.print(vector2[i]+" ");
+        }
+		System.out.println("");
+		System.out.println("Resultado del vector 2:");
+		System.arraycopy(vector, 0, vector2, 0, vector.length);
+		for ( int i = 0; i < vector2.length; i++ )
+		System.out.print(vector2[i] + " ");
+		
+	}
+	public void esMiembro() {
+		int introducido;
+
+		System.out.println("Pon el numero que quieras buscar en este conjunto: ");
+		introducido=teclado.nextInt();
+		Arrays.sort(vector);
+		if(Arrays.binarySearch(vector, introducido)!=0 ) {
+			System.out.println("El numero esta");
 		}
 	}
 	
-	public static void imprimir (ArrayList<Integer>num) {// imprime los nuemeros
-		for (int i = 0; i < num.size(); i++) {
-			System.out.println(num.get(i));
-		}
-	}
 }
+
+
--- Ej_6_Conjunto/src/Ejecutable.java
+++ Ej_6_Conjunto/src/Ejecutable.java
@@ -8,6 +8,44 @@ public class Ejecutable {
 
 	public static void main(String[]Args) {
 		Scanner teclado=new Scanner(System.in);
+		Conjunto nuevoConjunto=new Conjunto();
+		
+		boolean salir=true;
+		int eleccion;
+		nuevoConjunto.crear();
+		nuevoConjunto.crear2();
+do {
+			System.out.println("");
+			System.out.println("Indique 1 para imprimir, 2 para vaciar, 3 para añadir un numero, 4 para eliminar un numero,");
+			System.out.println("5 para copiar el primer vector en otro, 6 te dice si el numero esta, 7 para salir");
+				eleccion=teclado.nextInt();
+				
+			switch(eleccion) {
+			case 1 :
+				nuevoConjunto.decirVector();
+				break;
+			case 2:
+				nuevoConjunto.vaciar();
+				break;
+			case 3:
+				nuevoConjunto.añadir();
+				break;
+			case 4:
+				nuevoConjunto.eliminar();
+				break;
+			case 5:
+				nuevoConjunto.copia();
+				break;
+			case 6:
+				nuevoConjunto.esMiembro();
+				break;
+			case 7:
+				salir=false;
+				break;
+				
+				default: System.out.println("Porfavor, no rompas los huevos e introduce un valor del 1 al 7 (INCLUIDOS).");
+			}
+		}while (salir==true);
 		
 		
 	}
